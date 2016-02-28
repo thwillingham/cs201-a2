be3c6e6b064e5cc2ec4d5a207c92ca9fc07f1123
Thomas Willingham <thomas@tshows.us>
2016-02-28 07:29:47 +0300

    BST insert and printing are functioning correctly

---
+++ trees.java
@@ -0,0 +1,20 @@
+public class trees {
+    public static void main(String[] args) {
+        Bst tree = new Bst();
+        tree.insert("the");
+        tree.insert("quick");
+        tree.insert("brown");
+        tree.insert("fox");
+        tree.insert("jumped");
+        tree.insert("over");
+        tree.insert("the");
+        tree.insert("girl");
+        tree.insert("and");
+        tree.insert("her");
+        tree.insert("lazy");
+        tree.insert("lazy");
+        tree.insert("dog");
+        tree.print();
+    
+    }
+}
