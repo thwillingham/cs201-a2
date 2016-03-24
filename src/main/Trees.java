489f79f4ba4b916e060b5fc51adc863fe64e83a6
twillingham <twillingham@crimson.ua.edu>
2016-03-24 09:39:31 -0500

    added files in correct structure

---
+++ src/main/Trees.java
@@ -0,0 +1,64 @@
+package main;
+
+import trees.Bst;
+import trees.Rbt;
+import helpers.Importer;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Trees {
+    public static void main(String[] args) {
+        Bst tree = null;
+        if (args.length != 3) {
+            System.out.println("Error: invalid number of parameters");
+            System.exit(0);
+        }
+        
+        if (args[0].equals("-1")) {
+            tree = new Bst();
+        } else if (args[0].equals("-2")) {
+            tree = new Rbt();
+        } else {
+            System.out.println("Error: invalid tree type");
+            System.exit(0);
+        }
+        String contentsFile = args[1];
+        String commandsFile = args[2];
+
+        Importer i = new Importer(contentsFile, false);
+        String[] contents = i.getTokens();
+        tree.insert(contents);
+
+
+        Importer j = new Importer(commandsFile, true);
+        String[] tok = j.getTokens();
+        Queue<String> commands = new LinkedList<String>(Arrays.asList(tok));
+        
+        while (!commands.isEmpty()) {
+            String temp = commands.remove();
+            if (temp.equals("i")) {
+                System.out.println("#inserting");
+                tree.insert(commands.remove());
+            } else if (temp.equals("d")) {
+                System.out.println("#deleting");
+                tree.delete(commands.remove());
+            } else if (temp.equals("f")) {
+                System.out.println("#finding");
+                tree.find(commands.remove());
+            } else if (temp.equals("s")) {
+                System.out.println("#showing");
+                tree.print();
+            } else if (temp.equals("r")) {
+                System.out.println("#reporting");
+                tree.report();
+            } else {
+                System.out.println("Error: invalid command order");
+                System.exit(0);
+            }
+        }
+
+
+    }
+
+}
---
+++ src/trees/Rbt.java
@@ -0,0 +1,342 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.awt.Color;
+
+
+
+public class Rbt extends Bst {
+    protected class Node extends Bst.Node {
+        Color color = Color.red;
+
+        public Node(String s) {
+            super(s);
+        }
+
+        public Color getColor() {
+            return this.color;
+        }
+
+        public void setColor(Color c) {
+            this.color = c;
+        }
+
+        public boolean isRed() {
+            return (this.color == Color.red);
+        }
+
+        public void setColorRed() {
+            this.color = Color.red;
+        }
+
+        public boolean isBlack() {
+            return (this.color == Color.black);
+        }
+
+        public void setColorBlack() {
+            this.color = Color.black;
+        }
+
+        public Node getGrandparent() {
+            if (this == null || getParent() == null || getParent().getParent() == getParent()) {
+                return null;
+            } else {
+                return (Node)getParent().getParent();
+            }
+        }
+
+        public Node getUncle() {
+            if (this == null || getGrandparent() == null) {
+                return null;
+            } else if (getGrandparent().getLeftChild() == getParent()) {
+                return (Node)getGrandparent().getRightChild();
+            } else {
+                return (Node)getGrandparent().getLeftChild();
+            }
+        }
+
+        public Node getSibling() {
+            if (this == null || getParent() == null) {
+                return null;
+            } else if (getParent().getLeftChild() == this) {
+                return (Node)getParent().getRightChild();
+            } else {
+                return (Node)getParent().getLeftChild();
+            }
+        }
+
+        public void rotateLeft() {
+            if (this.getRightChild() == null) {
+                return;
+            }
+            Node curr = this;
+            Node prev = ((Node)this.getRightChild());
+            curr.setRightChild(prev.getLeftChild());
+            if (curr.getParent() == null || curr.getParent() == curr) {
+                setRootNode(prev);
+            } else if (curr.getParent().getLeftChild() == curr) {
+                curr.getParent().setLeftChild(prev);
+            } else {
+                curr.getParent().setRightChild(prev);
+            }
+            prev.setLeftChild(curr);
+        }
+
+        public void rotateRight() {
+          if (this.getLeftChild() == null) {
+              return;
+          }
+          Node curr = this;
+          Node prev = ((Node)this.getLeftChild());
+          curr.setLeftChild(prev.getRightChild());
+          if (curr.getParent() == null || curr.getParent() == curr) {
+              setRootNode(prev);
+          } else if (curr.getParent().getLeftChild() == curr) {
+              curr.getParent().setLeftChild(prev);
+          } else {
+              curr.getParent().setRightChild(prev);
+          }
+          prev.setRightChild(curr);
+        }
+
+    }
+
+    public Rbt() {
+        super();
+    }
+
+    @Override
+    public void insert(String s) {
+        this.insert(new Node(s));
+        return;
+    }
+
+    public void insert(Node n) {
+
+        if (this.rootNode == null) {
+            this.rootNode = n;
+            ((Node) this.rootNode).setColorBlack();
+            size++;
+        } else {
+            Node curr = ((Node)super.insert(n));
+            insertionFixUp(curr);
+        }
+    }
+
+    @Override
+    public void insert(String[] c) {
+        for (String s : c) {
+            this.insert(s);
+        }
+    }
+
+    public boolean isNotLinear(Node curr) {
+    	if (curr.getParent() == null || curr.getGrandparent() == null) {
+    		return false;
+    	}
+    	boolean lin = true;
+    	if (curr.getParent().getLeftChild() == curr && curr.getGrandparent().getLeftChild() == curr.getParent()) {
+    		lin = false;
+    	} else if (curr.getParent().getRightChild() == curr && curr.getGrandparent().getRightChild() == curr.getParent()) {
+    		lin = false;
+    	}
+    	return lin;
+    }
+
+    public void insertionFixUp(Node n) {
+        if (n==null || n.getFrequency() > 1) {
+            ((Node) rootNode).setColorBlack();
+            return;
+        }
+
+        Node curr = n;
+        curr.setColorRed();
+        if (curr != null && rootNode != curr && ((Node)curr.getParent()).isRed()) {
+            if (curr.getUncle() != null && curr.getUncle().isRed()) {
+                ((Node)curr.getParent()).setColorBlack();
+                curr.getUncle().setColorBlack();
+                curr.getGrandparent().setColorRed();
+                insertionFixUp(curr.getGrandparent());
+            } else if (curr.getParent() == curr.getGrandparent().getLeftChild()) {
+                if (curr == curr.getParent().getRightChild()) {
+                    curr = ((Node)curr.getParent());
+                    curr.rotateLeft();
+                }
+                ((Node)curr.getParent()).setColorBlack();
+                curr.getGrandparent().setColorRed();
+                ((Node) curr.getGrandparent()).rotateRight();
+            } else if (curr.getParent() == curr.getGrandparent().getRightChild()) {
+                if (curr == curr.getParent().getLeftChild()) {
+                    curr = ((Node)curr.getParent());
+                    curr.rotateRight();
+                }
+                ((Node)curr.getParent()).setColorBlack();
+                if (curr.getGrandparent() != null) {
+                    curr.getGrandparent().setColorRed();
+                    curr.getGrandparent().rotateLeft();
+                }
+            }
+        }
+        ((Node)rootNode).setColorBlack();
+    }
+
+    @Override
+    public void delete(String s) {
+        size--;
+        Node curr = (Node) getNode(s);
+        if (curr == null) {
+            size++;
+            return;
+        } else if (curr.getFrequency() > 1) {
+            curr.decrementFrequency();
+            size++;
+            return;
+        } else if (curr.getLeftChild() != null && curr.getRightChild() != null) {
+            Node predecessor = ((Node) curr.getPredecessor());
+            curr.setValue(predecessor.getValue());
+            curr.setFrequency(predecessor.getFrequency());
+            curr = (Node) predecessor;
+        }
+        Node move = null;
+        if (curr.getLeftChild() == null) {
+            move = ((Node) curr.getRightChild());
+        } else {
+            move = ((Node) curr.getLeftChild());
+        }
+        if (move != null) {
+            if (curr == rootNode) {
+                setRootNode(move);
+            } else if (curr.getParent().getLeftChild() == curr) {
+                curr.getParent().setLeftChild(move);
+            } else {
+                curr.getParent().setRightChild(move);
+            }
+            if (curr.isBlack()) {
+                deletionFixUp(move);
+            }
+        } else if (curr == rootNode) {
+            rootNode = null;
+        } else {
+            if (curr.isBlack()) {
+                deletionFixUp(curr);
+            }
+            // if (curr.getParent().getLeftChild() == curr) {
+            //     curr.getParent().setLeftChild(null);
+            // } else {
+            //     curr.getParent().setRightChild(null);
+            // }
+            // curr.setParent(null);
+            curr.prune();
+        }
+    }
+
+    public void deletionFixUp(Node x) {
+        while (x != rootNode && !(x.isRed())) {
+            if (x.getParent().getLeftChild() == x) {
+                Node sibling = x.getSibling();
+                if (sibling.isRed()) {
+                    sibling.setColorBlack();
+                    ((Node) x.getParent()).setColorRed();
+                    ((Node) x.getParent()).rotateLeft();
+                    sibling = (Node) x.getParent().getRightChild();
+                }
+                if (((Node) sibling.getLeftChild()).isBlack() && ((Node) sibling.getRightChild()).isBlack()) {
+                    sibling.setColorRed();
+                    x = ((Node) x.getParent());
+                } else {
+                    if (((Node) sibling.getRightChild()).isBlack()) {
+                        ((Node) sibling.getLeftChild()).setColorBlack();
+                        sibling.setColorRed();
+                        sibling.rotateRight();
+                        sibling = (Node) x.getParent().getRightChild();
+                    }
+                    sibling.setColor(((Node) x.getParent()).getColor());
+                    ((Node)  x.getParent()).setColorBlack();
+                    ((Node) sibling.getRightChild()).setColorBlack();
+                    ((Node) x.getParent()).rotateLeft();
+                    x = ((Node) rootNode);
+                }
+            } else {
+                Node sibling = (Node) x.getParent().getLeftChild();
+                if (sibling.isRed()) {
+                    sibling.setColorBlack();
+                    ((Node) x.getParent()).setColorRed();
+                    ((Node) x.getParent()).rotateRight();
+                    sibling = (Node) x.getParent().getLeftChild();
+                }
+                if (!sibling.hasBothChildren() || (((Node) sibling.getLeftChild()).isBlack() && ((Node)sibling.getRightChild()).isBlack())) {
+                    sibling.setColorRed();
+                    x = ((Node) x.getParent());
+                } else {
+                    if (sibling.getLeftChild() == null || ((Node) sibling.getLeftChild()).isBlack()) {
+                        ((Node) sibling.getRightChild()).setColorBlack();
+                        sibling.setColorRed();
+                        sibling.rotateLeft();
+                        sibling = x.getSibling();
+                    }
+                    sibling.setColor(((Node) x.getParent()).getColor());
+                    ((Node) x.getParent()).setColorBlack();
+                    ((Node) sibling.getLeftChild()).setColorBlack();
+                    ((Node) x.getParent()).rotateRight();
+                    x = ((Node) rootNode);
+                }
+            }
+        }
+        x.setColorBlack();
+    }
+
+    @Override
+    public void print() {
+        Queue<Node> q = new LinkedList<Node>();
+        if (rootNode == null) { return; }
+        q.add((Node)rootNode);
+        String stringToPrint = "";
+        Node parent = null;
+        int lineNumber = 1;
+        while (!q.isEmpty()) {
+            int count = q.size();
+            System.out.print(lineNumber + ": ");
+            while (count > 0) {
+                Node curr = q.remove();
+                if (curr.getLeftChild() == null && curr.getRightChild() == null) {
+                    stringToPrint = stringToPrint + "=";
+                }
+                if (rootNode == curr) {
+                	parent = ((Node) rootNode);
+                } else {
+                	parent = ((Node) curr.getParent());
+                }
+                stringToPrint = stringToPrint + curr.getValue();
+                if (curr.isRed()) {
+                    stringToPrint.concat("*");
+                    stringToPrint = stringToPrint + "*";
+                }
+                stringToPrint = stringToPrint + "(" + parent.getValue();
+                if (parent.isRed()) {
+                    stringToPrint = stringToPrint + "*";
+                }
+                stringToPrint = stringToPrint + ")" + curr.getFrequency();
+                if (curr == rootNode) {
+                    stringToPrint = stringToPrint + "X";
+                } else if (curr.getParent().getLeftChild() == curr) {
+                    stringToPrint = stringToPrint + "L ";
+                } else if (curr.getParent().getRightChild() == curr) {
+                    stringToPrint = stringToPrint + "R ";
+                }
+                System.out.print(stringToPrint);
+                if (curr.getLeftChild() != null) {
+                    q.add((Node)curr.getLeftChild());
+                }
+                if (curr.getRightChild() != null) {
+                    q.add((Node)curr.getRightChild());
+                }
+                count--;
+                stringToPrint = "";
+            }
+            System.out.println("");
+            lineNumber += 1;
+        }
+        return;
+    }
+}
